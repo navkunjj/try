435b2f5b58a3e45dd68b8e888bace18aa675f255
navkunjj <navkunjutpal@gmail.com.com>
2025-07-18 22:43:06 +0530

    Quick update

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn react with me bro 
+          Learn react with Navkunjj
         </a>
       </header>
     </div>
